ce68b9d2e2425701cf6b23ab4ae425b1bfce0867
Dimifabbridf96 <dimi.fabbri@gmail.com>
2023-02-08 23:54:26 +0000

    added styles to the navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,20 @@
 import React from 'react'
+import styles from '../styles/NavBar.module.css'
 import {Container, Nav, Navbar} from 'react-bootstrap';
 import icon from '../assets/marketplace-icon.png'
 
 const NavBar = () => {
   return (
-    <Navbar expand='md' fixed='top'>
+    <Navbar expand='md' fixed='top' className={styles.NavBar}>
         <Container>
         <Navbar.Brand href="#home"><img src={icon} alt='Icon' height='55' width='55'></img> MarketPlace
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link >Home</Nav.Link>
-            <Nav.Link >Sign In</Nav.Link>
-            <Nav.Link >Sign Up</Nav.Link>
+          <Nav className="ml-auto text-left">
+            <Nav.Link ><i class="fa-solid fa-house-user"></i>Home</Nav.Link>
+            <Nav.Link > <i class="fa-regular fa-user"></i> Sign In</Nav.Link>
+            <Nav.Link > <i class="fa-solid fa-pen"></i> Sign Up</Nav.Link>
 
           </Nav>
         </Navbar.Collapse>

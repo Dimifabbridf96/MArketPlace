8479a31593adc02130388a8f071e86c2ac77ac86
Dimifabbridf96 <dimi.fabbri@gmail.com>
2023-02-11 15:26:36 +0000

    added NavLink to the NavBar component, added routing to App.js, working on restyle NavLink

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import logo from './logo.svg';
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
+import { Container } from 'react-bootstrap';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
+    
     <div className={styles.App}>
       <NavBar />
+      <Container className={styles.Padding}>
+        <Switch>
+          <Route exact path='/' render={() => <h1>Home</h1>}/>
+          <Route exact path='/signin' render={() => <h2> SignIn</h2>}/>
+          <Route exact path='/signup' render={() => <h2> SignUp</h2>}/>
+        </Switch>
+      </Container>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,19 +2,24 @@ import React from 'react'
 import styles from '../styles/NavBar.module.css'
 import {Container, Nav, Navbar} from 'react-bootstrap';
 import icon from '../assets/marketplace-icon.png'
+import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
+    
     <Navbar expand='md' fixed='top' className={styles.NavBar}>
         <Container>
-        <Navbar.Brand href="#home"><img src={icon} alt='Icon' height='55' width='55'></img> MarketPlace
-        </Navbar.Brand>
+          <NavLink to='/'>
+        
+        <Navbar.Brand><img src={icon} alt='Icon' height='55' width='55'></img> MarketPlace
+        </Navbar.Brand> </NavLink>
+        
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
-            <Nav.Link ><i class="fa-solid fa-house-user"></i>Home</Nav.Link>
-            <Nav.Link > <i class="fa-regular fa-user"></i> Sign In</Nav.Link>
-            <Nav.Link > <i class="fa-solid fa-pen"></i> Sign Up</Nav.Link>
+            <NavLink to='/' className={styles.NavLink} activeClassName={styles.Active}><i className="fa-solid fa-house-user"></i>Home</NavLink>
+            <NavLink to='/signin' className={styles.NavLink} activeClassName={styles.Active} > <i className="fa-regular fa-user"></i> Sign In</NavLink>
+            <NavLink to='/signup' className={styles.NavLink} activeClassName={styles.Active} > <i className="fa-solid fa-pen"></i> Sign Up</NavLink>
 
           </Nav>
         </Navbar.Collapse>

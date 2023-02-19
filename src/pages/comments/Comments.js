3b06aa6621a5e2ada524b58933c10a1521d2be4a
Dimifabbridf96 <dimi.fabbri@gmail.com>
2023-02-19 20:23:15 +0000

    added comment structure toi comment component

--- src/pages/comments/Comments.js
+++ src/pages/comments/Comments.js
@@ -1,19 +1,30 @@
 import React from 'react'
+import { Media } from 'react-bootstrap';
 import styles from '../../styles/Comment.module.css'
 
 
 const Comments = (props) => {
     const{
-        id,
         owner,
         updated_at,
-        product,
         comment,
         profile_id,
         profile_image,
     }= props;
   return (
-    <div>Comments</div>
+    <div>
+        <hr />
+        <Media>
+        <Link to={`/profiles/${profile_id}`}>
+          <Avatar src={profile_image} />
+        </Link>
+        <Media.Body>
+            <span className={styles.Owner}>{owner}</span>
+            <span className={styles.Date}>{updated_at}</span>
+        <p> {comment }</p>
+        </Media.Body>
+        </Media>
+          </div>     
   )
 }
 

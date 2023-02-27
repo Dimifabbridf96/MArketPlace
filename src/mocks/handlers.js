89cdc648bfb3695a2e979ed8605b96b0dbebe92f
Dimifabbridf96 <dimi.fabbri@gmail.com>
2023-02-27 22:27:28 +0000

    added test handlers and setup for server

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -4,6 +4,18 @@ const baseURL = "https://marketplaceapi.herokuapp.com/"
 
 export const handlers = [
     rest.get(`${baseURL}dj-rest-auth/user/`, (req, res,ctx) => {
-        return res(ctx.json())
+        return res(ctx.json({
+            pk: 4,
+            username: "dimi",
+            email: "",
+            first_name: "",
+            last_name: "",
+            profile_id: 4,
+            profile_image: "https://res.cloudinary.com/de9jezwh1/image/upload/v1/media/../default-avatar-icon"
+            })
+            );
+    }),
+    rest.post(`${baseURL}dj-rest-auth/logout/`, (req,res,ctx) => {
+        return res(ctx.status(200));
     })
 ]

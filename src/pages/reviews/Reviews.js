ee25338a8a5367691f7c966e60154acf7f6cd4d5
Dimifabbridf96 <dimi.fabbri@gmail.com>
2023-02-27 15:44:27 +0000

    added basic structure to review page component

--- src/pages/reviews/Reviews.js
+++ src/pages/reviews/Reviews.js
@@ -2,12 +2,31 @@ import React from 'react'
 
 const Reviews = (props) => {
     const{
-        
-    }
+        owner,
+        created_at,
+        updated_at,
+        product,
+        review,
+        profile_id,
+        profile_image,
+        id,
+    }= props;
 
 
   return (
-    <div>Reviews</div>
+    <>
+        <hr />
+        <Media>
+          <Link to={`/profiles/${profile_id}`}>
+            <Avatar src={profile_image} />
+          </Link>
+          <Media.Body className="align-self-center ml-2">
+            <span className={styles.Owner}>{owner}</span>
+            <span className={styles.Date}>{updated_at}</span>
+            <p> { review }</p>
+            </Media.Body>
+        </Media>
+    </>
   )
 }
 
